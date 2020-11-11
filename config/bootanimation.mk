772724deb03c6c38def0c7f90fafa853010f3d19
mTresk <djtresk@gmail.com>
2020-11-11 19:32:42 +0100

    Revert "vendor: Add a custom boot animation for YAAP"
    This reverts commit 3026a5bbb7ac57bc401e1af084a02ad2caffe773.

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,4 +1,2 @@
-ifneq ($(wildcard vendor/yaap/prebuilt/common/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
-    vendor/yaap/prebuilt/common/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-endif
+    vendor/yaap/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
